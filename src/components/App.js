439597cd9742cd77bc49436fe3e16f35f2c68a04
Gionas <hello@gionas.dev>
2021-05-25 23:54:15 +0200

    Structure Sidebar and Main

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Sidebar from './Sidebar';
+import Main from './Main';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Sidebar />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
