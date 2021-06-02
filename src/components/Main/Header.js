af1dacbe8cbf840104044f19e9a3e5d2a98a88e6
Gionas <hello@gionas.dev>
2021-06-02 21:04:44 +0200

    Crypto info

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Header = ({ crypto }) => {
   return (
-    <header>
-      <div className="flex space-x-5 items-center">
+    <header className="flex space-y-5 justify-between items-center">
+      <div className="flex  space-x-5 items-center">
         <img src={crypto?.image} className="w-8 h-8" />
         <h1 className="text-3xl">{crypto?.name}</h1>
       </div>
+      <div>Current price: ${crypto?.current_price}</div>
+      <div>Market cap: ${crypto?.market_cap}</div>
+      <div>Volume: ${crypto?.total_volume}</div>
     </header>
   );
 };
