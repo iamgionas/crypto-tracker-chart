2b27c676a9a98b11a86d9bf20e4b46c879ca2234
Gionas <hello@gionas.dev>
2021-06-04 22:11:11 +0200

    Header changes

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -1,16 +1,43 @@
 import React from 'react';
 
 const Header = ({ crypto }) => {
+  const toUSD = (number) =>
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(number);
+
+  const getColor =
+    crypto?.market_cap_change_percentage_24h.toString().charAt(0) == '-'
+      ? 'bg-red-600'
+      : 'bg-green-600';
+
   return (
-    <header className="flex space-y-5 justify-between items-center">
-      <div className="flex  space-x-5 items-center">
-        <img src={crypto?.image} className="w-8 h-8" />
-        <h1 className="text-3xl">{crypto?.name}</h1>
-      </div>
-      <div>Current price: ${crypto?.current_price}</div>
-      <div>Market cap: ${crypto?.market_cap}</div>
-      <div>Volume: ${crypto?.total_volume}</div>
-    </header>
+    <main>
+      {crypto && (
+        <header className="grid grid-cols-4">
+          <div className="flex  space-x-5 items-center">
+            <img src={crypto?.image} className="w-8 h-8" />
+            <h1 className="text-3xl">{crypto.name}</h1>
+            <span
+              className={`${getColor} text-xs bg-gray-500 p-1 rounded`}
+            >{`${crypto.market_cap_change_percentage_24h.toFixed(2)}%`}</span>
+          </div>
+          <div className="grid justify-items-center">
+            <span>Current price</span>{' '}
+            <span class="font-bold text-xl">{toUSD(crypto.current_price)}</span>
+          </div>
+          <div className="grid justify-items-center">
+            <span>Market cap</span>{' '}
+            <span class="font-bold text-xl">{toUSD(crypto.market_cap)}</span>
+          </div>
+          <div className="grid justify-items-center">
+            <span>Volume</span>{' '}
+            <span class="font-bold text-xl">{toUSD(crypto.total_volume)}</span>
+          </div>
+        </header>
+      )}
+    </main>
   );
 };
 
