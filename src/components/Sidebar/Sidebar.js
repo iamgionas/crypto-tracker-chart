1a35363925c90b776c4526d9b3c30ba8642e036e
Gionas <hello@gionas.dev>
2021-06-06 19:46:23 +0200

    Sidebar scrollable

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,29 +4,31 @@ import Searchbar from './Searchbar';
 
 const Sidebar = () => {
   return (
-    <div className="bg-blue-900 w-1/6 space-y-10 py-7 px-2">
-      <a
-        href="#"
-        className="text-white flex items-center justify-center space-x-2"
-      >
-        <svg
-          className="w-8 h-8"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+    <div className="h-screen overflow-scroll bg-blue-900 w-1/6">
+      <div className="bg-blue-900 w-1/6 pb-4 fixed space-y-5">
+        <a
+          href="#"
+          className="text-white flex items-center justify-center space-x-2"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
-          />
-        </svg>
-        <span className="text-2x1 font-extrabold">Crypto Tracker Chart</span>
-      </a>
-      <Searchbar className="px-2" />
-      <Navbar className="px-2 text-white" />
+          <svg
+            className="w-8 h-8"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
+            />
+          </svg>
+          <span className="text-2x1 font-extrabold">Crypto Tracker Chart</span>
+        </a>
+        <Searchbar className="px-2" />
+      </div>
+      <Navbar className="my-32 px-2 text-white" />
     </div>
   );
 };
