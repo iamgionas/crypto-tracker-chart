86c842cce07edff3d71d9563ebdb3856418da133
Gionas <hello@gionas.dev>
2021-07-31 21:56:23 +0200

    CSS Order + Page Crypto

--- src/components/pages/CryptoSelected.js
+++ src/components/pages/CryptoSelected.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Header from '../layout/Header/Header';
 import Main from '../layout/Main/Main';
-import TextField from '../layout/TextField/TextField';
-import Grid from '../layout/Grid/Grid';
+import Chart from '../layout/Chart/Chart';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.development';
 
 const CryptoSelected = () => {
+  const { cryptoId } = useParams();
+
+  useEffect(() => {}, [cryptoId]);
+
   return (
     <>
       <Header>
-        <TextField />
+        <Link to="/">Back</Link> / {cryptoId}
       </Header>
       <Main>
-        <Grid />
+        <Chart />
       </Main>
     </>
   );
